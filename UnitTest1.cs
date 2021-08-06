09fe9a9d9cf9726f24f818a2b490df06013b7578
Rafał Korzeniec <51083650+RafKorz@users.noreply.github.com>
2021-08-06 11:39:03 +0200

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,44 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WikipediaTestingProject
+{
+    public class Tests
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver(@"C:\Users\User\source\repos\WikipediaTestingProject\WikipediaTestingProject\Resources\");
+            driver.Manage().Window.Position = new System.Drawing.Point(8, 30);
+            driver.Manage().Window.Size = new System.Drawing.Size(1290, 730);
+
+            driver.Manage().Timeouts().ImplicitWait = System.TimeSpan.FromSeconds(10);
+            driver.Manage().Timeouts().PageLoad = System.TimeSpan.FromSeconds(30);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            driver.Navigate().GoToUrl("https://www.wikipedia.org/");
+            IWebElement searchField = driver.FindElement(By.Id("searchInput"));
+            string searchEntry = "Czêstochowa";
+            searchField.SendKeys(searchEntry);
+            searchField.Submit();
+
+            string title = "Czêstochowa – Wikipedia, wolna encyklopedia";
+            driver.FindElement(By.XPath("/html/body/div[3]/form/fieldset/button/i")).Click();
+
+            string entryURL = "https://pl.wikipedia.org/wiki/Cz%C4%99stochowa";
+            Assert.AreEqual(entryURL, driver.Url, "URL id not correct");
+        }
+
+        [TearDown]
+        public void QuitDriver()
+        {
+            driver.Quit();
+        }
+    }
+}
